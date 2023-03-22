312f7ef785b8560fb71445383b6f05eeb55e5bae
Gustavo <gustgouvea@hotmail.com>
2023-03-22 10:46:33 -0300

    icons

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,5 +1,24 @@
+import { FaFacebook, FaInstagram, FaLinkedinIn } from "react-icons/fa";
+
+import styles from './Footer.module.css'
+
 function Footer() {
-  return <footer>Rodapé</footer>
+  return (
+    <footer>
+      <ul className={styles.social_list}>
+        <li>
+          <FaFacebook />
+        </li>
+        <li>
+          <FaInstagram />
+        </li>
+        <li>
+          <FaLinkedinIn />
+        </li>
+      </ul>
+      <p>Nosso Rodapé</p>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
